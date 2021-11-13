d488c024bf92bfb9a787f7a7fd1d0d01213118eb
woaiyhl <2824054196@qq.com>
2021-11-13 11:20:36 +0800

    '添加30-seconds-js'

---
+++ 10.30-seconds-js/01.CSVToArray.js
@@ -0,0 +1,13 @@
+
+const CSVToArray = (data, delimiter = ',', omitFirstRow = false) =>
+  data
+    .slice(omitFirstRow ? data.indexOf('\n') + 1 : 0)
+    .split('\n')
+    .map((v) => v.split(delimiter))
+
+console.log(CSVToArray('a,b\nc,d')) // [['a', 'b'], ['c', 'd']];
+console.log(CSVToArray('a;b\nc;d', ';')) // [['a', 'b'], ['c', 'd']];
+console.log(
+  CSVToArray('col1,col2\na,b\nc,d', ',', true),
+) // [['a', 'b'], ['c', 'd']];
+``
