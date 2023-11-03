bc0a6a3f90c75db7451d7e37e9efcffe3798b7ab
woaiyhl <2824054196@qq.com>
2023-11-04 06:48:14 +0800

    添加easyui

--- 12.js积累/57.test.js
+++ 12.js积累/57.test.js
@@ -1,25 +1,18 @@
-async function async1() {
-  console.log("async1 start");
-  await async2();
-  console.log("async1 end");
+/*
+ * @Author: woaiyhl 2824054196@qq.com
+ * @Date: 2023-07-13 10:59:07
+ * @LastEditors: woaiyhl 2824054196@qq.com
+ * @LastEditTime: 2023-11-03 21:07:54
+ * @FilePath: /myCode/js/js/12.js积累/57.test.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+Array.prototype.myPush = function (...args) {
+  for (let i = 0; i < args.length; i++) {
+    this[this.length] = args[i]
+  }
+  return this.length
 }
 
-async function async2() {
-  console.log("async2");
-}
-
-console.log("script start");
-
-setTimeout(function () {
-  console.log("setTimeout");
-}, 0);
-
-async1();
-
-new Promise(resolve => {
-  console.log("promise1");
-  resolve();
-}).then(function () {
-  console.log("promise2");
-});
-console.log('script end')
+let arr = [1, 2, 3, 4, 5]
+let len = arr.myPush([7, 8])
+console.log(arr, len)
