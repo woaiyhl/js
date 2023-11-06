f129dcb439d9b2ea8cb29b8b829483bb16ae3bc1
woaiyhl <2824054196@qq.com>
2023-11-06 20:12:34 +0800

    update

--- 12.js积累/57.test.js
+++ 12.js积累/57.test.js
@@ -1,11 +1,3 @@
-/*
- * @Author: woaiyhl 2824054196@qq.com
- * @Date: 2023-07-13 10:59:07
- * @LastEditors: woaiyhl 2824054196@qq.com
- * @LastEditTime: 2023-11-03 21:07:54
- * @FilePath: /myCode/js/js/12.js积累/57.test.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 Array.prototype.myPush = function (...args) {
   for (let i = 0; i < args.length; i++) {
     this[this.length] = args[i]
@@ -14,5 +6,5 @@ Array.prototype.myPush = function (...args) {
 }
 
 let arr = [1, 2, 3, 4, 5]
-let len = arr.myPush([7, 8])
+let len = arr.myPush(6, 7, 8)
 console.log(arr, len)
